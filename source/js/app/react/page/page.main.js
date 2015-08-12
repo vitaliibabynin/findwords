d7d0445e08f9daa0e65f03dcde618f78b22356b2
Vitaliy <vitaliy_babynin@yahoo.ca>
2015-08-12 14:45:06 +0300

    swiper compilation

--- source/js/app/react/page/page.main.js
+++ source/js/app/react/page/page.main.js
@@ -12,6 +12,7 @@ var Button = require('./../component/app.button').Button;
 var ScoreCounter = require('./../component/app.counter').ScoreCounter;
 var CoinsCounter = require('./../component/app.counter').CoinsCounter;
 
+var Swiper = require('./../../../app/libs/swiper');
 var Switch = require('./../component/app.switch').Switch;
 
 
@@ -28,7 +29,23 @@ var PageMain = Object.assign({}, {}, {
     },
 
     componentDidMount: function () {
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'vertical',
+            loop: true,
 
+            // If we need pagination
+            pagination: '.swiper-pagination',
+
+            // Navigation arrows
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+
+            // And if we need scrollbar
+            scrollbar: '.swiper-scrollbar'
+        })
+
+        return mySwiper;
     },
 
     componentDidUpdate: function (prevProps, prevState) {
@@ -86,8 +103,25 @@ var PageMain = Object.assign({}, {}, {
                         <CoinsCounter value={9999} />
                     </div>
                     <div className="main">
-                        <div className="swiper">
-                            /*swiper-element*/
+                        <!-- Slider main container -->
+                        <div class="swiper-container">
+                            <!-- Additional required wrapper -->
+                            <div class="swiper-wrapper">
+                                <!-- Slides -->
+                                <div class="swiper-slide">Slide 1</div>
+                                <div class="swiper-slide">Slide 2</div>
+                                <div class="swiper-slide">Slide 3</div>
+                                ...
+                            </div>
+                            <!-- If we need pagination -->
+                            <div class="swiper-pagination"></div>
+
+                            <!-- If we need navigation buttons -->
+                            <div class="swiper-button-prev"></div>
+                            <div class="swiper-button-next"></div>
+
+                            <!-- If we need scrollbar -->
+                            <div class="swiper-scrollbar"></div>
                         </div>
                         <div className="navigation">
                             <div className="settings">
