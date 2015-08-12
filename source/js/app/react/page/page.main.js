2d5f83baf69edeb094b02ccc9b3ebd48366f3f4d
Vitaliy <vitaliy_babynin@yahoo.ca>
2015-08-12 15:49:09 +0300

    className

--- source/js/app/react/page/page.main.js
+++ source/js/app/react/page/page.main.js
@@ -34,18 +34,14 @@ var PageMain = Object.assign({}, {}, {
             direction: 'horizontal',
             loop: true,
 
-
             pagination: '.swiper-pagination',
-
 
             nextButton: '.swiper-button-next',
             prevButton: '.swiper-button-prev',
 
-
             scrollbar: '.swiper-scrollbar'
-        });
 
-        return mySwiper;
+        });
     },
 
     componentDidUpdate: function (prevProps, prevState) {
@@ -103,20 +99,20 @@ var PageMain = Object.assign({}, {}, {
                         <CoinsCounter value={9999} />
                     </div>
                     <div className="main">
-                        <div class="swiper-container">
+                        <div className="swiper-container">
 
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide">Slide 1</div>
-                                <div class="swiper-slide">Slide 2</div>
-                                <div class="swiper-slide">Slide 3</div>
+                            <div className="swiper-wrapper">
+                                <div className="swiper-slide">Slide 1</div>
+                                <div className="swiper-slide">Slide 2</div>
+                                <div className="swiper-slide">Slide 3</div>
                             </div>
 
-                            <div class="swiper-pagination"></div>
+                            <div className="swiper-pagination"></div>
 
-                            <div class="swiper-button-prev"></div>
-                            <div class="swiper-button-next"></div>
+                            <div className="swiper-button-prev"></div>
+                            <div className="swiper-button-next"></div>
 
-                            <div class="swiper-scrollbar"></div>
+                            <div className="swiper-scrollbar"></div>
 
                         </div>
                         <div className="navigation">
