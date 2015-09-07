578b7dbdfeecad2235ea234dd31dc7ecce2c1d78
Vitaliy <vitaliy_babynin@yahoo.ca>
2015-09-07 19:49:17 +0300

    ad switch positioned

--- source/js/app/react/page/page.main.js
+++ source/js/app/react/page/page.main.js
@@ -1,12 +1,12 @@
-/** @jsx React.DOM */
 "use strict";
+
+
 var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
 var GameMixin = require('./../component/app.mixin').GameMixin;
 
 var Object = {assign: require('react/lib/Object.assign')};
 var classNames = require('classnames');
 
-var FbButton = require('./../component/app.button').FbButton;
 var Button = require('./../component/app.button').Button;
 
 var ScoreCounter = require('./../component/app.counter').ScoreCounter;
@@ -14,21 +14,25 @@ var CoinsCounter = require('./../component/app.counter').CoinsCounter;
 
 var Swiper = require('./../component/app.swiper').Swiper;
 var Navigation = require('./../component/app.menu').Navigation;
-var Switch = require('./../component/app.switch').Switch;
+var SwitchButton = require('./../libs/react-switch-button');
+
+
+var AD_SWITCH = "ad_switch";
 
 
 var PageMain = Object.assign({}, {}, {
+
     mixins: [PureRenderMixin, GameMixin],
     displayName: 'PageMain',
 
     swiper: null,
 
     getInitialState: function () {
-        var state = {
 
-        };
+        var state = {};
 
         return state;
+
     },
 
     componentDidMount: function () {
@@ -49,29 +53,39 @@ var PageMain = Object.assign({}, {}, {
             backgroundImage: "url(" + this.getImagePath(headImgName) + ")"
         };
 
+        require('./../libs/react-switch-button');
+
         return (
+
             <div className="page-main">
+
                 <div className="page-content">
+
                     <div className="head"
-                    style={headStyle}>
+                         style={headStyle}>
                     </div>
+
                     <div className="counters">
-                        <ScoreCounter value={appManager.getGameState().getScore()} />
-                        <CoinsCounter value={appManager.getGameState().getCoins()} />
+                        <ScoreCounter value={appManager.getGameState().getScore()}/>
+                        <CoinsCounter value={appManager.getGameState().getCoins()}/>
                     </div>
+
                     <div className="main">
                         <Swiper />
                         <Navigation />
                     </div>
+
                     <div className="footer">
-                        <Switch />
+                        <div className="text">{i18n._('switch.ad')}</div>
+                        <SwitchButton id={AD_SWITCH} defaultChecked="checked"/>
                     </div>
+
                 </div>
+
             </div>
 
         );
     }
 });
-
 module.exports = React.createClass(PageMain);
 module.exports.Class = PageMain;
